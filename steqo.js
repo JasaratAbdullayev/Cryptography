09739fbf1a03a9d99bdfc9c1ad626fb4207608e4
JasaratAbdullayev <127691825+JasaratAbdullayev@users.noreply.github.com>
2024-03-08 17:50:39 +0400

    Add files via upload

---
+++ steqo.js
@@ -0,0 +1,17 @@
+// script.js
+
+// Example JavaScript code
+//document.addEventListener('DOMContentLoaded', function () {
+ //   alert('Welcome to my simple webpage!');
+//});
+//
+
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
